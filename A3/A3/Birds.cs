a3428e2c54614d8c18c0474baa7375ad47545435
rounakpatel <78542032+rounakpatel@users.noreply.github.com>
2021-10-19 22:36:04 -0400

    a3

---
+++ A3/A3/Birds.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+using A3.Eggs;
+namespace A3.Birds
+{
+	class Bird
+	{
+		public static Random Rand = new Random(1);
+		public virtual Egg[] LayEggs(int numEggs)
+		{
+			Console.Error.WriteLine("Bird.LayEggs should not be called!");
+			return new Egg[0];
+		}
+	}
+
+	class Chicken : Bird
+	{
+     public override Egg[] LayEggs (int numEggs)
+
+			for (int i = 0; i < numEggs; ++1)
+	{
+	  double eggsize = Math.Round(Rand.NextDouble() * (4 - 2) + 2, 1);
+		// egg
+		Egg egg = new Egg(eggsize, Eggs.Egg.Colors.brown);
+
+		// broken egg
+		BrokenEgg Br_egg = new BrokenEgg(eggsize, Eggs.BrokenEgg.Colors.brown);
+	}
+
+	class Ostrich : Bird 
+	{
+		public override Egg[] LayEggs(int numEggs)
+		{ 
+
+			for (int i = 0; i < numEggs; ++1)
+			{
+				double eggsize = Math.Round(Rand.NextDouble() * (15 - 10) + 10, 1);
+				// egg
+				Egg egg = new Egg(eggsize, eggsize.Egg.Colors.speckled);
+
+				// broken egg
+				BrokenEgg Br_egg = new BrokenEgg(eggsize, eggsize.BrokenEgg.Colors.speckled);
+	}
+}
---
+++ A3/A3/Eggs.cs
@@ -0,0 +1,41 @@
+﻿namespace A3.Eggs
+{
+	class Egg
+	{
+		public enum Colors
+		{
+			speckled,
+			brown
+		}
+		public double Size { get; private set; }
+		public Colors Color { get; private set; }
+		public Egg(double size, Colors color)
+		{
+			Size = size;
+			Color = color;
+		}
+		public override string ToString()
+		{
+			return $"A {Size:0.0}cm {Color} egg";
+		}
+	}
+
+	class BrokenEgg : Egg
+	{
+		public enum Colors
+		{
+			speckled,
+			brown
+		}
+		public double Size { get; private set; }
+		public Colors Color { get; private set; }
+		public BrokenEgg(double size, Colors color)
+		{
+			Size = size;
+			Color = color;
+		}
+        public override string ToString()
+        {
+			return $"A {Size:0.0}cm {Color} broken egg";
+        }
+    }
